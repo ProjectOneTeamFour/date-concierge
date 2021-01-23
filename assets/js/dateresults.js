8eb8c394b240e600df8453684e197ac8653fae8d
samiul1988 <samiulhaydereee@gmail.com>
2021-01-23 02:22:14 -0700

    added zomato and geolocation apis, added materialize css framework

--- assets/js/dateresults.js
+++ assets/js/dateresults.js
@@ -1,2 +1,62 @@
+const ZOMATO_API_KEY = '670c5f2e4824fb60c96ab79528421baf';
+const OPEN_WEATHER_MAP_API_KEY = '';
+
 var attribites = document.location.search;
 console.log(attribites);
+
+// Extract parameters
+var params = (new URL(document.location)).searchParams;
+var username = params.get('username');
+var date = params.get('date');
+var mood = params.get('mood');
+
+var userLocation = {};
+
+var getLocation = function() {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+}
+
+var getRestaurantList = function(lat, lng, searchRadius) {
+    if(navigator.geolocation) {
+        getLocation()
+            .then(function(location) {
+                var lat = location.coords.latitude;
+                var lng = location.coords.longitude;
+                userLocation = {
+                    lat: location.coords.latitude,
+                    lng: location.coords.longitude,
+                }
+                
+                console.log(userLocation);
+
+                var url = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lng}&radius=${searchRadius}`;
+                var config = {
+                    headers: {
+                        "user-key": ZOMATO_API_KEY
+                    }
+                };
+                return fetch(url, config)
+            })
+            .then(function(response) {
+                if(response.ok){
+                    return response.json();
+                } else {
+                    alert("Error:" + response.statusText); // TO-DO: need to convert this to modal
+                }
+            })
+            .then(function(data) {
+                if(!data){
+                    return;
+                }
+                console.log(data);
+            })
+            .catch(error => console.log(error));
+    }       
+}
+
+getRestaurantList();
+
+// $("#search").on("click", getList);
+
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,7 +10,7 @@ var changePage = function(event)
     var userName = userNameEl.value;
     var date = dateEl.value;
     var mood = moodEl.value;
-
+    console.log(date);
     if(userName && date && mood)
     {
         document.location.replace("./dateresults.html"+"?username="+userName+"&date="+date+"&mood="+mood);
@@ -20,5 +20,60 @@ var changePage = function(event)
         console.log("error");
     }
 };
+
+var getLocation = function() {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+}
+
+var getRestaurantList = function(lat, lng, searchRadius) {
+    if(navigator.geolocation) {
+        getLocation()
+            .then(function(location) {
+                var lat = location.coords.latitude;
+                var lng = location.coords.longitude;
+                userLocation = {
+                    lat: location.coords.latitude,
+                    lng: location.coords.longitude,
+                }
+                
+                console.log(userLocation);
+
+                var url = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lng}&radius=${searchRadius}`;
+                var config = {
+                    headers: {
+                        "user-key": ZOMATO_API_KEY
+                    }
+                };
+                return fetch(url, config)
+            })
+            .then(function(response) {
+                if(response.ok){
+                    return response.json();
+                } else {
+                    alert("Error:" + response.statusText); // TO-DO: need to convert this to modal
+                }
+            })
+            .then(function(data) {
+                if(!data){
+                    return;
+                }
+                console.log(data);
+            })
+            .catch(error => console.log(error));
+    }       
+}
+
+// getRestaurantList();
+
+
+$(function(){
+    $("#date").datepicker();
+});
+
+$(document).ready(function(){
+    $('select').select();
+});
 
 submitFromEl.addEventListener("submit",changePage);

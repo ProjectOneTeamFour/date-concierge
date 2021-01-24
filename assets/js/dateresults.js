cf62524346503ce091aac723bf3c73be4e8c76ba
raedaltaki <raed.simon@gmail.com>
2021-01-24 13:47:09 -0500

    get movies categories and resturants cuisines from user mood

--- assets/js/dateresults.js
+++ assets/js/dateresults.js
@@ -50,7 +50,76 @@ var getWeatherData = function()
 //output: movie category , food type
 var getMoodTypes = function() 
 {
+    var adventurous = 
+    {
+        genres : ["Action","Adventure","Crime","Documentary","History","Horro","Mystery","Science Fiction","Thriller","War","Western","Drama","Fantasy"],
+        cuisines : [
+                    "Afghan","Afghani","African","Arabian","Argentine","Armenian","Australian","Austrian",
+                    "Bangladeshi","Brazilian","British","Bubble Tea","Burmese","Cajun","California","Cambodian",
+                    "Cantonese","Caribbean","Central Asian","Colombian","Cuban","Dim Sum","Eastern European",
+                    "Egyptian","Ethiopian","European","Filipino","First Nations","Fusion","Georgian","German",
+                    "Greek","Hakka Chinese","Hawaiian","Hong Kong Style","Hungarian","Indian","Indonesian",
+                    "International","Iranian","Irish","Israeli","Jamaican","Japanese","Jewish","Korean",
+                    "Latin American","Lebanese","Malaysian","Mediterranean","Mexican","Middle Eastern",
+                    "Modern European","Moldovan","Mongolian","Moroccan","Nepalese","New Mexican","Nigerian",
+                    "North Indian","Northern Chinese","Oriental","Pacific Northwest","Pakistani","Pan Asian",
+                    "Patisserie","Persian","Peruvian","Quebecois","Ramen","Russian","Salad","Salvadorean",
+                    "Thai","Tibetan","Trinbagonian","Turkish","Ukrainian","Vegetarian","Venezuelan","Vietnamese",
+                    "West Indian","Xinjiang","Yunnan","Polish","Portuguese","Swedish","Swiss","Syrian","Taiwanese",
+                    "Tanzanian","South Indian","Southern","Southwestern","Spanish","Sri Lankan","Shanghai",
+                    "Sichuan","Singaporean","Somali","Scottish"
+                    ]
+    };
+    var relaxed = 
+    {
+        genres : ["Animation","Comedy","Family","Music","TV Movie"],
+        cuisines : [
+                    "American","Asian","Asian Fusion","BBQ","Bagels","Bakery","Bar Food","Beverages",
+                    "Brasserie","Breakfast","Burger","Cafe","Chinese","Coffee and Tea","Canadian",
+                    "Continental","Deli","Desserts","Diner","Donuts","Drinks Only","Fast Food","Finger Food",
+                    "Fish and Chips","Frozen Yogurt","Grill","Healthy Food","Ice Cream","Juices","Kebab",
+                    "Pizza","Pub Food","Salad","Sandwich","Tea","Tex-Mex","Soul Food"
+                    ]
+    };
+    var romantic = 
+    {
+        genres : ["Romance"],
+        cuisines : ["Steak","Sushi","French","Crepes","Italian","Tapas","Seafood"]
+    };
+    var spontaneous = 
+    {
+        genres : adventurous.genres.concat(relaxed.genres,romantic.genres),
+        cuisines : adventurous.cuisines.concat(relaxed.cuisines,romantic.cuisines)
+    };
 
+    var result =
+    {
+        genres : "",
+        cuisines : ""
+    };
+
+    switch(mood)
+    {
+        case "spontaneous":
+            result.genre = spontaneous.genres[Math.floor(Math.random()*spontaneous.genres.length)];
+            result.cuisine = spontaneous.cuisines[Math.floor(Math.random()*spontaneous.cuisines.length)];
+            return result;
+
+        case "adventurous":
+            result.genre = adventurous.genres[Math.floor(Math.random()*adventurous.genres.length)];
+            result.cuisine = adventurouss.cuisines[Math.floor(Math.random()*adventurous.cuisines.length)];
+            return result;
+
+        case "relaxed":
+            result.genre = relaxed.genres[Math.floor(Math.random()*relaxed.genres.length)];
+            result.cuisine = relaxed.cuisines[Math.floor(Math.random()*relaxed.cuisines.length)];
+            return result;
+
+        case "romantic":
+            result.genre = romantic.genres[Math.floor(Math.random()*romantic.genres.length)];
+            result.cuisine = romantic.cuisines[Math.floor(Math.random()*romantic.cuisines.length)];
+            return result;
+    }
 }
 
 
@@ -86,7 +155,8 @@ var getRestaurantData = function()
 //output: All movie data
 var getMovieData = function() 
 {
-    movieGenre = "Comedy"; //remove and add genre from getMoodTypes() function
+    var movieGenre = getMoodTypes().genre;//"Comedy"; //remove and add genre from getMoodTypes() function
+    console.log(movieGenre);
     fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+ TMDB_API_KEY +"&language=en-US")
     .then(function(response)
     {
@@ -111,7 +181,6 @@ var getMovieData = function()
     .then(function(data)
     {
         var movie = data.results[counter];
-        console.log(movie);
         var imageEl = document.createElement("img");
         var imgURL =  movie.poster_path || movie.backdrop_path;
         imageEl.src = "https://image.tmdb.org/t/p/original" + imgURL;
@@ -121,7 +190,7 @@ var getMovieData = function()
         var voteEl = document.createElement("p");
         voteEl.textContent ="Rate: " + movie.vote_average + "/10";
         var releaseDateEl = document.createElement("p");
-        releaseDateEl.textContent ="Release Date: " + movie.release_date;
+        releaseDateEl.textContent ="Release Date: " + movie.release_date;     
         var overviewEl = document.createElement("p");
         overviewEl.textContent ="Overview: " + movie.overview;
 
@@ -138,7 +207,6 @@ var getMovieData = function()
 
         movieMapContainerEl.appendChild(movieImgEl);
         movieMapContainerEl.appendChild(movieDetailsEl);
-        console.log(movie.genre_ids);
     });
 }
 
@@ -199,7 +267,7 @@ var nextBtnHandler = function()
 {
     counter++;
     movieMapContainerEl.innerHTML ="";
-    // getMovieData();
+    getMovieData();
 }
 
 //save list
@@ -229,7 +297,7 @@ var loadList = function()
 //     // instance.open();
 // }); 
 
-// getMovieData();
+getMovieData();
 nextBtnEl.addEventListener("click", nextBtnHandler);
 saveBtnEl.addEventListener("click", addToList);
 

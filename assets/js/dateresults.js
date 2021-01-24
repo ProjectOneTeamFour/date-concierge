7655b28a1a3be6b2c610e4e5c4e0edbcb402dfc1
raedaltaki <raed.simon@gmail.com>
2021-01-23 22:42:09 -0500

    clear up the code and make function

--- assets/js/dateresults.js
+++ assets/js/dateresults.js
@@ -1,5 +1,12 @@
-var attribites = document.location.search;
-console.log(attribites);
+// Elements
+var nextBtnEl = document.querySelector("#next-btn");
+var saveBtnEl = document.querySelector("#save-btn");
+
+
+//API Keys
+const ZOMATO_API_KEY = '670c5f2e4824fb60c96ab79528421baf';
+const OWM_API_KEY = "ce455bd02a51e6ea3ab658a42367d8f2";
+
 
 // Extract parameters
 var params = (new URL(document.location)).searchParams;
@@ -8,5 +15,163 @@ var date = params.get('date');
 var mood = params.get('mood');
 
 
+//variables
+var userLocation = {}; // stores user location
+
+
+
+//get location function
+//input : user accept
+//output: coordinates
+var getLocation = function() 
+{
+    // return new Promise(function(resolve, reject) 
+    // {
+    //     navigator.geolocation.getCurrentPosition(resolve, reject);
+    // })
+}
+
+
+//get weather data
+//input : coordinates , date 
+//output: tempreture
+var getWeatherData = function() 
+{
+    // let urlForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${parseFloat(userLocation.lat)}&lon=${parseFloat(userLocation.lng)}&units=metric&exclude=minutely,hourly,alerts&appid=${OWM_API_KEY}`;
+    // return fetch(urlForecast);
+}
+
+
+//get types of resturants and movies based on the mood
+//input : user mood
+//output: movie category , food type
+var getMoodTypes = function() 
+{
+
+}
+
+
+//get resturants data
+//input : coordinates, food type
+//output: All resturant data
+var getRestaurantData = function() 
+{
+    // var lat = location.coords.latitude;
+    // var lng = location.coords.longitude;
+    // var searchRadius = 1000;
+
+    // userLocation = 
+    // {
+    //     lat: location.coords.latitude,
+    //     lng: location.coords.longitude
+    // };
+
+    // var url = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lng}&radius=${searchRadius}`;
+    // var config = 
+    // {
+    //     headers: 
+    //     {
+    //         "user-key": ZOMATO_API_KEY
+    //     }
+    // };
+    // return fetch(url, config);
+}
+
+
+//get resturants data
+//input : movie category
+//output: All movie data
+var getMovieData = function() 
+{
+
+}
+
+
+// choose indoor or outdoor then display the results
+var displayResults = function() 
+{
+    // if(navigator.geolocation) 
+    // {
+    //     getLocation()
+    //         .then(getRestaurantList)
+    //         .then(function(response) 
+    //         {
+    //             if(response.ok)
+    //             {
+    //                 return response.json();
+    //             } 
+    //             else 
+    //             {
+    //                 alert("Error:" + response.statusText); // TO-DO: need to convert this to a modal
+    //             }
+    //         })
+    //         .then(function(data) 
+    //         {
+    //             if(!data)
+    //             {
+    //                 return;
+    //             }
+    //             console.log("Restaurant Data", data);
+    //             return getWeatherData();
+    //         })
+    //         .then(function(response) 
+    //         {
+    //             if(response.ok)
+    //             {
+    //                 return response.json();
+    //             } 
+    //             else 
+    //             {
+    //                 alert("Error:" + response.statusText); // TO-DO: need to convert this to a modal
+    //             }
+    //         })
+    //         .then(function(data) 
+    //         {
+    //             if(!data)
+    //             {
+    //                 return;
+    //             }
+    //             console.log("Weather Data", data);
+    //             document.location.replace("./dateresults.html"+"?username="+userName+"&date="+date+"&mood="+mood);
+    //         })
+    //         .catch(error => console.log(error));
+    // }       
+}
+
+// show next movie and resturant
+var nextBtnHandler = function()
+{
+
+}
+
+//save list
+var addToList = function()
+{
+    
+}
+
+//load list
+var loadList = function()
+{
+    
+}
+
+//open Modal
+
+// document.addEventListener('DOMContentLoaded', function() 
+// {
+//     var elems = document.querySelectorAll('.modal');
+//   //  var instances = M.Modal.init(elems, options);
+//     // instances.open();
+// });
+// // Or with jQuery
+// $(document).ready(function()
+// {
+//     $('.modal').modal();
+//     // instance.open();
+// });
+          
+nextBtnEl.addEventListener("click", nextBtnHandler);
+saveBtnEl.addEventListener("click", addToList);
 
 
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,112 +1,44 @@
 var submitFromEl = document.querySelector("#submit-form");
 var userNameEl = document.querySelector("#user-name");
 var dateEl = document.querySelector("#date");
-var moodEl = document.querySelector("#date-mood");
+var moodEl = document.querySelector("#mood");
 
-const ZOMATO_API_KEY = '670c5f2e4824fb60c96ab79528421baf';
-const OWM_API_KEY = "ce455bd02a51e6ea3ab658a42367d8f2";
-var userLocation = {}; // stores user location
 var userName = '';
 var date = '';
 var mood = '';
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
-
-var getLocation = function() {
-    return new Promise(function(resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    })
-}
-
-var getRestaurantList = function(location) {
-    var lat = location.coords.latitude;
-    var lng = location.coords.longitude;
-    var searchRadius = 1000;
-
-    userLocation = {
-        lat: location.coords.latitude,
-        lng: location.coords.longitude
-    };
-
-    var url = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lng}&radius=${searchRadius}`;
-    var config = {
-        headers: {
-            "user-key": ZOMATO_API_KEY
-        }
-    };
-    return fetch(url, config);
-}
-
-var getWeatherData = function() {
-    let urlForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${parseFloat(userLocation.lat)}&lon=${parseFloat(userLocation.lng)}&units=imperial&exclude=minutely,hourly,alerts&appid=${OWM_API_KEY}`;
-    return fetch(urlForecast);
-}
-
-var getData = function() {
-    if(navigator.geolocation) {
-        getLocation()
-            .then(getRestaurantList)
-            .then(function(response) {
-                if(response.ok){
-                    return response.json();
-                } else {
-                    alert("Error:" + response.statusText); // TO-DO: need to convert this to a modal
-                }
-            })
-            .then(function(data) {
-                if(!data){
-                    return;
-                }
-                console.log("Restaurant Data", data);
-                return getWeatherData();
-            })
-            .then(function(response) {
-                if(response.ok){
-                    return response.json();
-                } else {
-                    alert("Error:" + response.statusText); // TO-DO: need to convert this to a modal
-                }
-            })
-            .then(function(data) {
-                if(!data){
-                    return;
-                }
-                console.log("Weather Data", data);
-                document.location.replace("./dateresults.html"+"?username="+userName+"&date="+date+"&mood="+mood);
-            })
-            .catch(error => console.log(error));
-    }       
-}
 
 var changePage = function(event)
 {
     event.preventDefault();
 
+    console.log(moodEl);
     userName = userNameEl.value;
     date = dateEl.value;
     mood = moodEl.value;
+
+    console.log(userName,date,mood);
     
-    if(userName && date && mood) {
-        getData();
-        // document.location.replace("./dateresults.html"+"?username="+userName+"&date="+date+"&mood="+mood);
-    } else {
-        console.log("error");
+    if(userName && date && mood) 
+    {
+        document.location.replace("./dateresults.html"+"?username="+userName+"&date="+date+"&mood="+mood);
+    } 
+    else 
+    {
+        console.log("error"); // replace with forum
     }
 };
 
-
 // Initialize datepicker
-$(function(){
-    $("#date").datepicker();
+$("#date").datepicker(
+{
+    format: "dd-mm-yyyy",
+    minDate : new Date(),
 });
 
 // Initialize select
-$(document).ready(function(){
-    $('select').formSelect();
-});
+$('select').formSelect();
 
 submitFromEl.addEventListener("submit", changePage);
+
+
+
